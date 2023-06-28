4b551269de044eea5e3b467b581cb9405f552abd
Alexandre Gutierry <aleguti1000@hotmail.com>
2023-06-28 19:09:33 -0300

    prisma and simple configs

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,10 @@
-import {app} from './app'
-app.listen({
-  host: '0.0.0.0',
-  port: 3333
-}).then(
-  () => {
+import { app } from './app'
+import { env } from './env'
+app
+  .listen({
+    host: '0.0.0.0',
+    port: env.PORT,
+  })
+  .then(() => {
     console.log('🎈Server listening on port 3333')
-  }
-)
+  })
